67c161b44c44ff7c6eb4e040d2d9cfc49fb73b0a
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2023-12-26 15:37:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,24 +64,13 @@ st.title("Stock Financial Statement Analysis for Specific Dates")
 # User input for stock search
 stock_to_search = st.text_input("Enter Stock Name to Search:")
 
-# Date range selection
-start_date = st.date_input("Select Start Date:")
-end_date = st.date_input("Select End Date:")
-
-# Convert date inputs to the desired format
-start_date_str = start_date.strftime("%b-%y")
-end_date_str = end_date.strftime("%b-%y")
-
-# Selected dates within the specified range
-selected_dates = [
+# Selected date
+selected_date = st.selectbox("Select Date:", [
     "Dec-15", "Mar-16", "Jun-16", "Sep-16", "Dec-16", "Mar-17", "Jun-17", "Sep-17",
     "Dec-17", "Mar-18", "Jun-18", "Sep-18", "Dec-18", "Mar-19", "Jun-19", "Sep-19", "Dec-19",
     "Mar-20", "Jun-20", "Sep-20", "Dec-20", "Mar-21", "Jun-21", "Sep-21", "Dec-21", "Mar-22",
     "Jun-22", "Sep-22", "Dec-22", "Mar-23", "Jun-23", "Sep-23"
-]
-
-# Filter selected dates based on the date range
-selected_dates = [date for date in selected_dates if start_date_str <= date <= end_date_str]
+])
 
 # Financial folder path
 financial_folder = "financial"
@@ -91,7 +80,7 @@ if st.button("Fetch Financial Statements"):
     st.write(f"Fetching Financial Statements for {stock_to_search} for Selected Dates...")
     
     # Read fundamental data for the searched stock
-    fundamental_data = read_fundamental_data(financial_folder, stock_to_search, selected_dates)
+    fundamental_data = read_fundamental_data(financial_folder, stock_to_search, [selected_date])
 
     if fundamental_data is not None:
         # Extract Income Statement data
@@ -99,27 +88,10 @@ if st.button("Fetch Financial Statements"):
         income_statement_df = pd.DataFrame(income_statements).set_index('Date')
 
         # Perform horizontal analysis (changes over time)
-        fig_horizontal = px.line(income_statement_df, x=income_statement_df.index, y=['Total Revenue/Income', 'Total Operating Expense', 'Operating Income/Profit', 'EBITDA', 'Net Income'],
-                      title=f"Horizontal Analysis for {stock_to_search}",
+        fig_horizontal = px.line(income_statement_df, x=income_statement_df.columns, y=income_statement_df.loc[selected_date],
+                      title=f"Horizontal Analysis for {stock_to_search} on {selected_date}",
                       labels={'value': 'Amount'},
                       line_shape="linear",
                       markers=True)
 
         st.plotly_chart(fig_horizontal)
-
-        # Perform vertical analysis (percentage of total) for the latest date
-        latest_date = income_statement_df.index[-1]
-        latest_data = income_statement_df.loc[latest_date]
-        total_revenue = latest_data['Total Revenue/Income']
-        
-        # Avoid division by zero
-        if total_revenue != 0:
-            percentages = latest_data[['Total Operating Expense', 'Operating Income/Profit', 'EBITDA', 'Net Income']] / total_revenue * 100
-
-            # Create a bar chart for vertical analysis
-            fig_vertical = px.bar(percentages, x=percentages.index, y=percentages.columns,
-                          title=f"Vertical Analysis for {stock_to_search} on {latest_date}",
-                          labels={'value': 'Percentage'},
-                          barmode='stack')
-
-            st.plotly_chart(fig_vertical)
